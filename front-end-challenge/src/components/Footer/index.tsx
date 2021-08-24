b397f4ab1c53756be8d5b7f7c67c64392f99df56
matheus <matheuscomteaga@hotmail.com>
2021-08-24 16:02:24 -0300

    correção do e-mail

--- front-end-challenge/src/components/Footer/index.tsx
+++ front-end-challenge/src/components/Footer/index.tsx
@@ -5,8 +5,8 @@ const Footer: React.FC = () => {
   return (
     <Container>
       <p>Desenvolvido por Matheus Pansani Pedroso</p>
-      <a href="mailto:matheus.pansani@outlook">
-        <IoMailOutline size={16} /> Contato: matheus.pansani@outlook
+      <a href="mailto:matheus.pansani@outlook.com">
+        <IoMailOutline size={16} /> Contato: matheus.pansani@outlook.com
       </a>
     </Container>
   );
